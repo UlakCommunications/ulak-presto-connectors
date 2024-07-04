4662c8c23d02bc65a37a78fbea72783e86ba8689
fatih.yuce <fatih.yuce@ulakhaberlesme.com.tr>
2024-07-04 13:10:30 +0300

    cache ops continued

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
  VERSION=$1
  ./mvnw clean package
- docker build -t 192.168.57.202:35000/maya/trino:432${VERSION} .
- docker push 192.168.57.202:35000/maya/trino:432${VERSION}
+ docker build -t 192.168.57.202:35000/maya/trino:${VERSION} .
+ docker push 192.168.57.202:35000/maya/trino:${VERSION}
