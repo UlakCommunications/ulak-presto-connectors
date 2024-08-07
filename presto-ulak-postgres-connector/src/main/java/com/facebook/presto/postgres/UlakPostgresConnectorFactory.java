b430a4291e0f60b9584e2716af13e6108911efe1
fatih.yuce <fatih.yuce@ulakhaberlesme.com.tr>
2024-08-07 14:34:12 +0300

    fix column medata bugs and reformat code structure.

---
+++ presto-ulak-postgres-connector/src/main/java/com/facebook/presto/postgres/UlakPostgresConnectorFactory.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.postgres;
+
+
+import io.trino.spi.connector.Connector;
+import io.trino.spi.connector.ConnectorContext;
+import io.trino.spi.connector.ConnectorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+import java.util.Map;
+
+//import static com.facebook.presto.influxdb.RedisCacheWorker.DEFAULT_N_THREADS;
+
+public class UlakPostgresConnectorFactory
+        implements ConnectorFactory
+{
+    public static final String TEXT_CONNECTOR_INFLUXDB = "mayapostgres";
+    private static Logger logger = LoggerFactory.getLogger(UlakPostgresConnectorFactory.class);
+
+    public String getName()
+    {
+        return TEXT_CONNECTOR_INFLUXDB;
+//        return TEXT_CONNECTOR_PG;
+    }
+
+//    @Override
+//    public ConnectorHandleResolver getHandleResolver()
+//    {
+//        return new UlakHandleResolver();
+//    }
+
+    @Override
+    public Connector create(String catalogName, Map<String, String> config, ConnectorContext context)
+    {
+        String url = config.get("connection-url");
+        String sNumThreads = config.get("number_of_worker_threads");
+        int numThreads =10;// DEFAULT_N_THREADS;
+        if(sNumThreads != null && !sNumThreads.trim().equals("")){
+            try {
+                numThreads = Integer.parseInt(sNumThreads);
+            }catch (Throwable e){
+                logger.error("Unable to parse sNumThreads: " + sNumThreads,e);
+            }
+        }
+        String sRunInCoordinatorOnly = config.get("run_in_coordinator_only");
+        boolean runInCoordinatorOnly = false;
+        if(sRunInCoordinatorOnly != null && !sRunInCoordinatorOnly.trim().equals(""))
+        {
+            runInCoordinatorOnly = sRunInCoordinatorOnly.trim().toLowerCase(Locale.ENGLISH).equals("true");
+        }
+        String sWorkerIndexToRunIn = config.get("worker_id_to_run_in");
+        UlakPostgresConnector connector = null;
+        switch ( getName()){
+            case TEXT_CONNECTOR_INFLUXDB:
+                connector = new UlakPostgresConnector(
+                        url,
+                        catalogName,
+                        config.get("connection-org"),
+                        config.get("connection-token"),
+                        config.get("connection-bucket"),
+                        config.get("redis-url"),
+                        config.get("keywords"),
+                        runInCoordinatorOnly,
+                        context.getNodeManager().getCurrentNode().getNodeIdentifier(),
+                        sWorkerIndexToRunIn,
+                        context.getNodeManager().getCurrentNode().isCoordinator(),
+                        numThreads,
+                        config.get("pg-connection-url"),
+                        config.get("pg-connection-user"),
+                        config.get("pg-connection-password"),
+                        config.get("qw-connection-url"),
+                        config.get("qw-index"));
+//            case TEXT_CONNECTOR_PG:
+//                connector = new InfluxdbConnector(DBType.PG,
+//                        url,
+//                        catalogName,
+//                        config.get("connection-org"),
+//                        config.get("connection-token"),
+//                        config.get("connection-bucket"),
+//                        config.get("redis-url"),
+//                        config.get("keywords"),
+//                        runInCoordinatorOnly,
+//                        context.getNodeManager().getCurrentNode().getNodeIdentifier(),
+//                        sWorkerIndexToRunIn,
+//                        context.getNodeManager().getCurrentNode().isCoordinator(),
+//                        numThreads,
+//                        config.get("pg-connection-url"),
+//                        config.get("pg-connection-user"),
+//                        config.get("pg-connection-password"));
+        }
+
+        return connector;
+    }
+}
