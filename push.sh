79724462f22e7a022b9940f36cb54e7fd945b53a
fatih.yuce <fatih.yuce@ulakhaberlesme.com.tr>
2025-12-10 14:47:59 +0300

    prod ip

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=$1
+version=$1
 platform=$2
 prod=$3
 prod_ip=$4
@@ -13,7 +13,7 @@ set -x
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
 
-nexus_repo="192.168.57.202:35000/maya/trino:${VERSION}"
+nexus_repo="192.168.57.202:35000/maya/trino:${version}"
 
 # Eğer prod seçilmişse, prod_nexus_repo ve nexus_repo push işlemi gerçekleşir
 if [ "$prod" == "true" ]; then
@@ -22,7 +22,7 @@ if [ "$prod" == "true" ]; then
         exit 1
     fi
     echo $prod_ip
-    prod_nexus_repo="${prod_ip}:35000/maya/maya-api-gateway:${version}"
+    prod_nexus_repo="${prod_ip}:35000/maya/trino:${version}"
 
     # 1- Eğer prod true ise, sadece Nexus repo'yu baz alarak build işlemi yapılır
     echo "Prod seçildi, sadece Nexus repo'yu baz alarak build yapılıyor."
